5396e9098bb8ea1e4b02e052fdad73010b0bcee3
Erika Silverio <cms.silverio@gmail.com>
2024-05-01 22:26:23 -0300

    Practicas en JavaScript

---
+++ js-01/js/script.js
@@ -0,0 +1,123 @@
+// console.log("Hola del .JS")
+
+
+//  VARIABLES ------------------------------ <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// let num;
+// num = 4;
+// console.log(num);
+
+// num = 7 ;
+// console.log(num);
+
+// num = "Peter's" ;
+// console.log(num);
+
+// num = ` A " B " C `  ;
+// console.log(num);
+
+// num = 2 * (4 + 6);
+// console.log(num);
+
+
+// let numero = 55 ;
+
+// let nombreCompleto = "Erika Silverio"
+// console.log(nombreCompleto);
+
+
+// CONSTANTE -------------------------<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// const PI = 3.1416;
+// const IVA = 10.5;
+
+// console.log(IVA);
+
+
+// TIPOS DE DATOS -------------- <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// let dato;
+// console.log(dato + 3);
+// console.log(dato , typeof dato);
+
+// dato = 5.3434;
+// console.log(dato , typeof dato);
+
+// dato = 5.3434;
+// console.log(dato , dato.toFixed(2), typeof dato);
+
+
+// dato = "Cinco";
+// console.log(dato , typeof dato);
+
+// dato = null;
+// console.log(dato , typeof dato);
+
+// dato = undefined;
+// console.log(dato , typeof dato);
+
+// dato = new Number (4.78545);
+// console.log(dato , dato.toFixed(2), typeof dato);
+
+
+// Parse -----------------------<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// let num = '34.56d7';
+
+// let numeroEntero = parseInt(num);
+// console.log(numeroEntero);
+
+// let numeroDecimal = parseFloat(num);
+// console.log(numeroDecimal);
+
+// let numero = Number(num);
+// console.log(numero);
+
+
+// OPERADORES ------------>>>>>>>>>>>>>>>>>.
+
+
+// let num1 = 12;
+// let num2 = 3;
+
+// console.log(num1 % num2);
+
+// let num3 = "12";
+// let num4 = "3";
+
+// console.log(num3 + num4);
+
+// let num5 = parseInt("12");
+// let num6 = parseInt("3");
+
+// console.log(num5 + num6);
+
+
+// let num7 = parseInt("20");
+// let num8 = parseInt("20");
+
+// let resultado = num7 + num8;
+// console.log(resultado);
+
+// resultado = resultado + 10;   ---> es el mismo
+// resultado += 10;
+
+
+// console.log(resultado)
+
+
+// prompt  ----------------<<<<<<<<<<<<<<<<<
+
+
+// let numero = parseInt(prompt('Ingrese un numero'));
+
+// let numero = prompt('Ingrese um numero');
+// numero = parseInt(numero);
+
+// console.log(numero, typeof numero);
+// console.log(numero + 12);
+
+// document.write(numero)
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
