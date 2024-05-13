061f209108dc0ad47dbe9b9457d93ddc851fe615
Erika Silverio <cms.silverio@gmail.com>
2024-05-13 20:57:36 -0300

    Seguimos con JS

---
+++ js-06/js/querySelector.js
@@ -0,0 +1,15 @@
+// let parrafo = document.querySelector(".destacado");
+// console.log(parrafo);
+
+// let parrafo = document.querySelector("p.destacado");
+// console.log(parrafo);
+
+
+
+let parrafo = document.querySelector("#segundo-parrafo");
+console.log(parrafo);
+
+let parrafos = document.querySelectorAll("p");
+// console.log(parrafos);
+
+parrafos.forEach(parrafo => console.log(parrafo));
