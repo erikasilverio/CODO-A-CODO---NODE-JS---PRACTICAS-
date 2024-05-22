153ecd6a9c51f1a173bf2aa681d5d396cba024a5
Erika Silverio <cms.silverio@gmail.com>
2024-05-21 21:26:52 -0300

    en JS

---
+++ js-06/js/createElement.js
@@ -0,0 +1,44 @@
+// const section = document.createElement('section');
+
+// const h2 =  document.createElement("h2");
+// const text = document.createTextNode("Productos");
+// h2.textContent = "Productos";
+
+// h2.appendChild(text);
+
+
+// section.appendChild(h2);
+
+// console.log(section);
+
+
+// let main = document.querySelector("main");
+// main.appendChild(section);
+
+// let main = document.querySelector("main");
+// main.innerHTML += "<section><h2> Productos </h2></section>"
+
+let main = document.querySelector("main");
+
+const title = "Productos";
+
+
+
+main.innerHTML += `
+    <section>
+        <h2> ${title} </h2>
+    </section>
+    `;
+
+
+const small = document.querySelector("small");
+small.textContent = "CopyWrite 2025";
+small.className = "footer_small";
+small.style.color = "orange";
+
+
+// const otroSmall = small.cloneNode(true);
+// otroSmall.textContent = 2025;
+
+
+console.log(small);
--- js-06/js/querySelector.js
+++ js-06/js/querySelector.js
@@ -7,9 +7,12 @@
 
 
 let parrafo = document.querySelector("#segundo-parrafo");
-console.log(parrafo);
+// console.log(parrafo);
 
-let parrafos = document.querySelectorAll("p");
+let parrafos = document.querySelectorAll("article div");
 // console.log(parrafos);
 
-parrafos.forEach(parrafo => console.log(parrafo));
+// parrafos.forEach(parrafo => console.log(parrafo));
+
+let label = document.querySelector('[for="nombre"]');
+console.log(label);
